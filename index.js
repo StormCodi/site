ea71b9e1eb8fd362fa27cf8c1a8382023e95d7ce
StormCodi <121410908+StormCodi@users.noreply.github.com>
2023-03-03 13:56:39 -0800

    Created a number pad

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function Clicked() {
+function Menu() {
     var hiddenDiv = document.getElementById('hidden');
     if (hiddenDiv.style.display === 'none') {
       hiddenDiv.style.display = 'flex';
@@ -6,4 +6,30 @@ function Clicked() {
       hiddenDiv.style.display = 'none';
     }
   }
+
+  var i = 0
   
+
+function lol(x){
+    if (document.getElementById(x).style.backgroundColor == "green") {
+        console.log(i);
+        i++;
+        document.getElementById(x).style.backgroundColor = "red";
+        document.getElementById(x).style.color = "red";
+
+    } else {
+        document.getElementById(x).style.backgroundColor = "green"
+        document.getElementById(x).style.color = "green";
+        console.log(i);
+        i++
+    }
+}
+
+function Numbers() {
+    var hiddenDiv = document.getElementById('NumberPad');
+    if (hiddenDiv.style.display === 'none') {
+      hiddenDiv.style.display = 'flex';
+    } else {
+      hiddenDiv.style.display = 'none';
+    }
+}
